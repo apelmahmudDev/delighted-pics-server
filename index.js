38af33f241103d6b6901de9c81717e4aff5cab43
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-08-30 15:02:54 +0600

    Updated

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// mongodb uri
+// mongodb config
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pn1pz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
+
 client.connect((err) => {
 	const serviceCollection = client.db('delighted-pics').collection('services');
 	const orderCollection = client.db('delighted-pics').collection('orders');
